48ba80c9fbb9bcb536cea30ed5a9c4a7ddf41c75
unknown <muhammadanas0261@gmail.com>
2026-01-15 15:36:00 +0500

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './sections/Home/Home'
+import Skills from './sections/Skills/Skills'
+import Contact from './sections/Contact/Contact'
+import ParticlesBackground from './components/ParticlesBackground'
+import About from './sections/About/About'
+import CustomCursor from './components/CustomCursor'
+import TechStack from './sections/TechStack/TeckStack'
+import ExperienceSection from './sections/Experience/Experience'
+import ProjectSection from './sections/Project/Projects'
+import Education from './sections/Education/Education'
+
 const App = () => {
   return (
     <div className="relative bg-black min-h-screen overflow-x-hidden">
@@ -6,8 +19,14 @@ const App = () => {
       <ParticlesBackground />
       <CustomCursor />
 
+      {/* 2. Main Container:
+           - max-w-7xl: Stops the site from getting too wide (simulates your 80% zoom view)
+           - mx-auto: Centers the site
+           - pt-28: Pushes content down so Navbar doesn't cover it
+      */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 pt-28">
         
+        {/* Navbar is here, but we might need to fix its width inside its own file too */}
         <Navbar />
         
         <Home />
@@ -23,3 +42,5 @@ const App = () => {
     </div>
   )
 }
+
+export default App
