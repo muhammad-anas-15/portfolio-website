f38c92ac3f3cd6e5a34d027cca0763eddf1dd504
unknown <muhammadanas0261@gmail.com>
2026-01-15 15:32:52 +0500

    Align navbar width with page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './sections/Home/Home'
-import Skills from './sections/Skills/Skills'
-import Contact from './sections/Contact/Contact'
-import ParticlesBackground from './components/ParticlesBackground'
-import About from './sections/About/About'
-import CustomCursor from './components/CustomCursor'
-import TechStack from './sections/TechStack/TeckStack'
-import ExperienceSection from './sections/Experience/Experience'
-import ProjectSection from './sections/Project/Projects'
-import Education from './sections/Education/Education'
-
 const App = () => {
   return (
     <div className="relative bg-black min-h-screen overflow-x-hidden">
       
-      {/* GLOBAL BACKGROUND - Keeps covering full screen */}
+      {/* 1. Background stays full screen (outside the container) */}
       <ParticlesBackground />
-
-      {/* CUSTOM CURSOR */}
       <CustomCursor />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 pt-28">
+        
         <Navbar />
+        
         <Home />
         <About />
         <TechStack />
@@ -36,5 +23,3 @@ const App = () => {
     </div>
   )
 }
-
-export default App
